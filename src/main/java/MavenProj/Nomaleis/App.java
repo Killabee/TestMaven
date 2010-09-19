6e502d5f4f700dff55ae346df552747744618e1b
= <=>
2010-09-19 20:55:46 +0200

    Maven First Push

---
+++ src/main/java/MavenProj/Nomaleis/App.java
@@ -0,0 +1,13 @@
+package MavenProj.Nomaleis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
